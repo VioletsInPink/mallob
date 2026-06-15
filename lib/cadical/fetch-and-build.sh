a0d240ed1bdb85df4a12226b12a2c4703ffd4e9d
Henri Kyi <henri.kiwi05@gmail.com>
2026-06-15 12:38:13 +0200

    CaDiCaL: using vivify_only branch for fetch

--- lib/cadical/fetch-and-build.sh
+++ lib/cadical/fetch-and-build.sh
@@ -3,8 +3,9 @@
 source ../base-build-functions.sh
 dirname="cadical"
 
-branchorcommit="be7a0f84190b3216c589696b2010e8cbf8a8252e" # updated 2026-02-05
-fetch_and_extract $dirname configure https://github.com/VioletsInPink/cadical/archive/${branchorcommit}.zip
+# branchorcommit="be7a0f84190b3216c589696b2010e8cbf8a8252e" # updated 2026-02-05
+branchorcommit="vivify_only"
+fetch_and_extract $dirname configure https://github.com/VioletsInPink/cadical/archive/refs/heads/${branchorcommit}.zip
 
 echo "[cadical] Building ..."
 ./configure
