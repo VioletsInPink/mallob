27e254f48d4d75a1d27014bdf0c04f20bc91e05c
Henri Kyi <henri.kiwi05@gmail.com>
2026-07-06 11:20:11 +0200

    skip recompute of stats

--- scripts/vivify_only/sat_benchmark.sh
+++ scripts/vivify_only/sat_benchmark.sh
@@ -76,7 +76,7 @@ max_timout="$(($timeout + 15))"
 
 # TODO Add further options to these arguments Mallob is called with.
 malloboptions="-t=$THREADS_PER_PROC -jwl=$timeout -T=$max_timout -v=3 -sleep=1000 -trace-dir=. -pipe-large-solutions=0 -processes-per-host=$NPROCS -regular-process-allocation -max-lits-per-thread=50000000 -strict-clause-length-limit=20 -clause-filter-clear-interval=500 -max-lbd-partition-size=2 -export-chunks=20 -satsolver=$portfolio -vivi=$vivify"
-echo $malloboptions
+# echo $malloboptions
 #####################################################################
 
 
@@ -95,6 +95,11 @@ if [ "$1" == "--extract" ]; then
     if [ -z $1 ]; then
         echo "Provide a results directory."
         exit 1
+    fi
+
+    if [ -f qualified-runtimes-and-results.txt ]; then
+        echo "results exist"
+        exit 0
     fi
 
     > $1/qualified-runtimes.txt
