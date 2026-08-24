d1065c0c3512a7874bab229211f200826917e1a0
Henri Kyi <henri.kiwi05@gmail.com>
2026-08-24 12:09:24 +0200

    enabled subsume

--- src/app/sat/solvers/cadical.cpp
+++ src/app/sat/solvers/cadical.cpp
@@ -199,7 +199,7 @@ void Cadical::diversify(int seed) {
     okay = solver->set("inprocessing", 1);
     okay &= solver->set("vivify", 1);
     okay &= solver->set("vivifyonly", 1);
-    okay &= solver->set("subsume", 0);
+    okay &= solver->set("subsume", 1);
     okay &= solver->set("transred", 0);
 
     okay &= solver->set("restart", 0);
